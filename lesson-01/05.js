597e4765a442dcf066aefb731a5795e8a5fcb3aa
MaximOEgorov <llkk67@gmail.com>
2023-12-06 19:08:40 +0300

    Lesson 1 task 5

---
+++ lesson-01/05.js
@@ -0,0 +1,14 @@
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (true) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения");
+        break;
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
+        break;
+    case rule.length === 25:
+        console.log("50 на 50");
+        break;
+    default:
+}
