bee13be4760d556f179fc69487490d1d293814c0
MaximOEgorov <llkk67@gmail.com>
2023-12-06 19:04:31 +0300

    Lesson 1 task 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,10 +1,6 @@
 let number = 1;
 
-console.log(number);
-number++;
-number++;
-console.log(number);
-console.log(number);
-number--;
-number--;
-console.log(number);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
