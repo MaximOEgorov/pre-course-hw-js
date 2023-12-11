5583089ad4ec914d3e4a2db232c65c3acf8a3f91
MaximOEgorov <llkk67@gmail.com>
2023-12-12 00:06:33 +0300

    lesson-02-6

---
+++ lesson-02/07.js
@@ -0,0 +1,11 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+let passportMarried2 = {...passportMarried, married:true, address: {...passportMarried.address}};
+console.log(passportMarried.married)
+console.log(passportMarried2.married)
