c4845cfb591741491d5d8be85816420ed5fd8f15
MaximOEgorov <llkk67@gmail.com>
2023-12-19 10:46:15 +0300

    Lesson 1 task 7

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return (colors.filter(color => (color === "черный" || color === "красный" || color === "желтый"))
+        .join("-"));
+}
+
+console.log(createColorString());
+
